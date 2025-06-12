c476579179546a339e9374500d8457b6cb2d1273
Varad Sandeep Naik <varad0007@gmail.com>
2025-06-13 00:24:41 +0530

    fixed the bug in ProjectDisplay Component - it wasnt rendering dynamic image

--- src/Components/Projects/ProjectDisplaySection.jsx
+++ src/Components/Projects/ProjectDisplaySection.jsx
@@ -11,31 +11,31 @@ const ProjectDisplaySection = () => {
 
     const Projects = [
         {
-            "Image" : "src/Components/Projects/Assets/1.png",
+            "Image" : Project1,
             "Title" : "QRHungry",
             "Description" : "QRhungry is your one-stop shop for creating QR codes that are as unique and flavorful as you are. Whether you're a business owner, marketer, or simply looking to add a creative touch to your personal projects, we empower you to generate codes that stand out and get noticed.",
             "Link" : "https://hellacious1702.github.io/QRhungry/"
         } , 
         {
-            "Image" : "src/Components/Projects/Assets/2.png",
+            "Image" : Project2,
             "Title" : "Apple Website Clone",
             "Description" : "My inaugural React.js project involves engineering a high-fidelity replica of the iconic Apple India website. This challenging undertaking is instrumental in solidifying my understanding of component-based architecture, advanced CSS techniques, and responsive web design principles. It's a hands-on demonstration of my commitment to mastering modern front-end development by translating complex UI/UX into a seamless, interactive experience.",
             "Link" : "https://hellacious1702.github.io/apple-website-clone-reactJS/"
         } ,
         {
-            "Image" : "src/Components/Projects/Assets/3.png",
+            "Image" : Project3,
             "Title" : "ShouldYou",
             "Description" : "Developed a foundational React.js project to create an interactive 'Yes/No Oracle' game. This application served as a practical exercise in mastering core React concepts such as component hierarchy, state management, and event handling, effectively illustrating the creation of dynamic user interfaces that respond to user queries with randomized, insightful answers.",
             "Link" : "https://hellacious1702.github.io/ShouldYou/"
         } , 
         {
-            "Image" : "src/Components/Projects/Assets/4.png",
+            "Image" : Project4,
             "Title" : "University Demo Website",
             "Description" : "Developed a University Demo Website using HTML, CSS, and JavaScript. This foundational web development project meticulously showcases front-end design, responsive layout, and structured web experiences. Hosted on GitHub Pages, it effectively demonstrates core coding and deployment proficiency.",
             "Link" : "https://hellacious1702.github.io/University-Demo-Webiste/"
         } ,
         {
-            "Image" : "src/Components/Projects/Assets/5.png",
+            "Image" : Project5,
             "Title" : "Global Search Handle",
             "Description" : "Developed an intuitive Web Search Simplifier with HTML, CSS, and JavaScript. It streamlines content discovery: users input queries and select platforms via dropdown for direct searching. This project enhances convenience and showcases core front-end development capabilities.",
             "Link" : "https://hellacious1702.github.io/Global-Search-Handle/"
