5c1bc3e432ede04efee3cd21cec9a42ede677a31
Varad Sandeep Naik <varad0007@gmail.com>
2025-06-13 20:42:57 +0530

    Made the website mobile responsive with a working NavBar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,16 +5,28 @@ import MailIcon from './Assets/Mail.png'
 import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons/faBarsStaggered';
 
 
 const Navbar = () => {
 
   const [activeNav , setActiveNav] = useState("Home");
+  const [activeMobileNav , setActiveMobileNav] = useState("Close");
   
     function handleNav(index)
     {
           setActiveNav(index);
+    }
+
+    function handleMobileNav(index)
+    {
+          if(index=="Open")
+          {
+            setActiveMobileNav("Open")
+          }else{
+            setActiveMobileNav("Close")
+          }
     }
 
   return (
@@ -33,6 +45,35 @@ const Navbar = () => {
           <div className="ContactWrapper">
               <Link to="/portfolio/Contact"><FontAwesomeIcon icon={faEnvelope} size="2xl" style={{color: "#24262f",}} /></Link>
           </div>
+
+          <div className="MobileNavButton" onClick={() => handleMobileNav(activeMobileNav == "Close" ? "Open" : "Close")}>
+            <FontAwesomeIcon icon={faBarsStaggered} size="xl" style={{color: "#24252f",}} />
+          </div>
+
+      </div>
+
+      <div className="MobileNavWrapper" id={activeMobileNav === "Close" ? "notActiveMobileNav" : "ActiveMobileNav"} >
+
+          <Link to="/portfolio/" id={activeNav=="Home" ? "Active" : "notActive"} onClick={() => {
+            handleNav("Home");
+            handleMobileNav("Close")
+          }}>Home</Link>
+
+          <Link to="/portfolio/Projects" id={activeNav=="Project" ? "Active" : "notActive"} onClick={() => {
+            handleNav("Project");
+            handleMobileNav("Close")
+          }}>Project</Link>
+
+          <Link to="/portfolio/About" id={activeNav=="About" ? "Active" : "notActive"} onClick={() => {
+            handleNav("About");
+            handleMobileNav("Close")
+          }}>About</Link>
+
+          <Link to="/portfolio/Contact" id={activeNav=="Contact" ? "Active" : "notActive"} onClick={() => {
+            handleNav("Contact");
+            handleMobileNav("Close")
+          }}>Get in Touch</Link>
+
       </div>
         
     </>
